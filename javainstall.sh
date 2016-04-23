df168e29f521cacef2d67c276c95eff35986ff0a
lusm <1357197829@qq.com>
2016-04-23 08:56:41 +0800

    添加了一个linux下自动环境配置脚本

---
+++ javainstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo su #切换到root权限
+mkdir /usr/local/java
+cd /usr/local/java
+
+#download jdk 1.7.67
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz
+
+#extract jdk
+tar -xvf jdk-7u67-linux-x64.tar.gz
+
+#set environment
+export JAVA_HOME="/usr/local/java/jdk1.7.0_67"
+if ! grep "JAVA_HOME=/usr/local/java/jdk1.7.0_67" /etc/environment 
+then
+  echo "JAVA_HOME=/usr/local/java/jdk1.7.0_67" | sudo tee -a /etc/environment 
+  echo "export JAVA_HOME" | sudo tee -a /etc/environment 
+  echo "PATH=$PATH:$JAVA_HOME/bin" | sudo tee -a /etc/environment 
+  echo "export PATH" | sudo tee -a /etc/environment 
+  echo "CLASSPATH=.:$JAVA_HOME/lib" | sudo tee -a /etc/environment 
+  echo "export CLASSPATH" | sudo tee -a /etc/environment 
+fi
+
+#update environment
+source /etc/environment  
+ehco "jdk is installed !"
